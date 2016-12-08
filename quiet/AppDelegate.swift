fc0ee492ab45af24c90c8717dd85d34b10ffa796
Ellen Li <ellen.li@shopify.com>
2016-12-08 15:47:08 -0500

    Initial Commit

---
+++ quiet/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  quiet
+//
+//  Created by Ellen Li on 2016-12-08.
+//  Copyright © 2016 Ellen Li. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
